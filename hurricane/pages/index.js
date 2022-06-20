025e028954daa8a4229941160a1a012eec6a03e4
Katrina Hill <khill365@gmail.com>
2022-06-20 12:06:10 -0700

    added base code for Upload.js and index.js

--- hurricane/pages/index.js
+++ hurricane/pages/index.js
@@ -1,9 +1,16 @@
+import Upload from './modules/Upload'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home()
+{
   return (
-    <h1>Hello!</h1>
+    <>
+      <h1>Iro Iro!</h1>
+      < Upload />
+    </>
   )
+
 }
+
