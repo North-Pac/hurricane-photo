5cc8628ec1ce5ee02e25a75224c6aa35eaf81f7c
Bishal Khanal <bishal.khanal87@gmail.com>
2022-06-22 13:39:45 -0700

    started creating forms/login

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
-import Upload from '../modules/Upload'
-import Header from '../modules/Header'
+import Upload from '../components/Upload'
+import Header from '../components/Header'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import LoginForm from '../components/LoginForm'
+import { useAuth } from '../contexts/auth'
+
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 
 export default function Home()
 {
+  const { user, login, logout } = useAuth();
+  
   return (
     <>
       <Header />
+      {!user && <LoginForm onSubmit={login}/>}
       < Upload />
+      
+      
     </>
   )
 
